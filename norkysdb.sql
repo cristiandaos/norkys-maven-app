eabe52ccbb26858d44552bfcdb6184df42a8411c
luis18vc <147955339+luis18vc@users.noreply.github.com>
2024-07-05 00:53:18 -0500

    bd_update
    tablas nuevas

--- norkysdb.sql
+++ norkysdb.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost:33065
--- Tiempo de generación: 22-05-2024 a las 10:19:45
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-07-2024 a las 07:52:13
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -33,8 +33,20 @@ CREATE TABLE `cliente` (
   `apellidos` varchar(150) NOT NULL,
   `telefono` varchar(15) DEFAULT NULL,
   `correo` varchar(130) NOT NULL,
-  `password` varchar(300) NOT NULL
+  `password` varchar(300) NOT NULL,
+  `roles` varchar(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id_cli`, `nombres`, `apellidos`, `telefono`, `correo`, `password`, `roles`) VALUES
+(1, 'Luis', 'Villalobos', '9999999', 'luis@mail.com', 'luis12', '1'),
+(3, 'Guillermo', 'Vasques', NULL, 'gui@mail.com', '1234', '3'),
+(5, 'Victor', 'Caceres', '121221', 'victor@mail.com', '1234', '3'),
+(7, 'juan', 'Castro', NULL, 'juan@mail.com', '1234', '2'),
+(8, 'Prueba', 'Ape', NULL, 'prueba@mail.com', '1234', '2');
 
 -- --------------------------------------------------------
 
@@ -53,15 +65,28 @@ CREATE TABLE `detalle_pedido` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `id_empleado` int(11) NOT NULL,
+  `nom_empleado` varchar(50) NOT NULL,
+  `apellido_emp` varchar(50) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `contraseña` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `pedido`
 --
 
 CREATE TABLE `pedido` (
   `id_ped` int(11) NOT NULL,
   `id_cli` int(11) NOT NULL,
-  `fecha_ped` datetime NOT NULL,
-  `total` decimal(8,2) NOT NULL,
-  `estado` varchar(60) DEFAULT NULL
+  `fecha_ped` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `total` decimal(8,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -91,6 +116,44 @@ INSERT INTO `producto` (`id_prod`, `nombre`, `precio`, `imagen`, `copy`) VALUES
 (6, '1/2 pollo a la brasa', 34.00, 'PROD000005.png', '- 1/2 pollo a la brasa\r\n- papas fritas\r\n- ensalada tradicional fresca\r\n- cremas\r\n'),
 (7, '1/2 pollo para 2 personas', 38.00, 'PROD000006.png', '- 1/4 de pollo a la brasa\r\n- papas fritas\r\n- ensalada tradicional fresca\r\n- cremas\r\n- 2 gaseosa 600ml'),
 (8, 'Pechuga Norkys', 19.90, 'PROD000007.png', '- Filete de pechuga de pollo con tocino, queso, champiñones y jamón\r\n- papas fritas\r\n- ensalada fresca');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_rol` int(11) NOT NULL,
+  `nombre_rol` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_rol`, `nombre_rol`) VALUES
+(1, 'Administrador'),
+(2, 'empleado'),
+(3, 'cliente');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `visitas`
+--
+
+CREATE TABLE `visitas` (
+  `id` int(11) NOT NULL,
+  `contador` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `visitas`
+--
+
+INSERT INTO `visitas` (`id`, `contador`) VALUES
+(1, 2);
 
 --
 -- Índices para tablas volcadas
@@ -125,6 +188,18 @@ ALTER TABLE `producto`
   ADD PRIMARY KEY (`id_prod`);
 
 --
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `visitas`
+--
+ALTER TABLE `visitas`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -132,7 +207,7 @@ ALTER TABLE `producto`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id_cli` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_pedido`
@@ -150,7 +225,13 @@ ALTER TABLE `pedido`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id_prod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id_prod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT de la tabla `visitas`
+--
+ALTER TABLE `visitas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
